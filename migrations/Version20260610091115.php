b3b2dc6b16876c98872a361bb093db62cc42592c
Heeralall <heeralall@MacBook-Air-de-Heeralall.local>
2026-06-10 11:17:32 +0200

    [sf-start] creation of genre factory

--- migrations/Version20260610091115.php
+++ migrations/Version20260610091115.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260610090502 extends AbstractMigration
+final class Version20260610091115 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,21 +20,16 @@ final class Version20260610090502 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE book (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, isbn VARCHAR(13) DEFAULT NULL, summary CLOB DEFAULT NULL, publication_date DATE DEFAULT NULL, available BOOLEAN NOT NULL, language VARCHAR(10) DEFAULT NULL, cover_url VARCHAR(255) DEFAULT NULL)');
         $this->addSql('CREATE TABLE book_genre (book_id INTEGER NOT NULL, genre_id INTEGER NOT NULL, PRIMARY KEY (book_id, genre_id), CONSTRAINT FK_8D92268116A2B381 FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_8D9226814296D31F FOREIGN KEY (genre_id) REFERENCES genre (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_8D92268116A2B381 ON book_genre (book_id)');
         $this->addSql('CREATE INDEX IDX_8D9226814296D31F ON book_genre (genre_id)');
         $this->addSql('CREATE TABLE genre (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(100) NOT NULL)');
-        $this->addSql('CREATE TABLE messenger_messages (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, body CLOB NOT NULL, headers CLOB NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL)');
-        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 ON messenger_messages (queue_name, available_at, delivered_at, id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE book');
         $this->addSql('DROP TABLE book_genre');
         $this->addSql('DROP TABLE genre');
-        $this->addSql('DROP TABLE messenger_messages');
     }
 }
