146f1f565d489789513e10f1f229a7c1e909d826
Adrien Roches <adrien@roc-it.tech>
2026-06-10 16:27:33 +0200

    [sf-start] Generate form for Book entity with symfony console make:form BookType Book

---
+++ src/Form/BookType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use App\Entity\Genre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('isbn')
+            ->add('summary')
+            ->add('publicationDate', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('available')
+            ->add('language')
+            ->add('coverUrl')
+            ->add('genres', EntityType::class, [
+                'class' => Genre::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('authors', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
