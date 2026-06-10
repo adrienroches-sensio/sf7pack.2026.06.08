6b31d3ff1db8fa2de2f1c97f300a7e441bdbbe7a
Adrien Roches <adrien@roc-it.tech>
2026-06-10 16:35:20 +0200

    [sf-start] Add BookController for admin + required template

---
+++ src/Controller/Admin/BookController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Entity\Book;
+use App\Form\BookType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BookController extends AbstractController
+{
+    #[Route('/admin/books/new', name: 'admin_book_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($book);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_book_show', ['id' => $book->getId()]);
+        }
+
+        return $this->render('admin/book/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
