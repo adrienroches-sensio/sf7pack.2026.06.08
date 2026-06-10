9c75ff9f2b74f55b2e7aea506599d5b8ee5e554b
Adrien Roches <adrien@roc-it.tech>
2026-06-10 16:30:35 +0200

    [sf-start] Add constraints on Book entity

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 class Book
@@ -16,15 +17,19 @@ class Book
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotNull()]
+    #[Assert\Length(min: 3, max: 255)]
     #[ORM\Column(length: 255)]
     private ?string $title = null;
 
+    #[Assert\Isbn(type: Assert\Isbn::ISBN_13)]
     #[ORM\Column(length: 13, nullable: true)]
     private ?string $isbn = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $summary = null;
 
+    #[Assert\LessThanOrEqual('today')]
     #[ORM\Column(type: Types::DATE_IMMUTABLE, nullable: true)]
     private ?\DateTimeImmutable $publicationDate = null;
 
